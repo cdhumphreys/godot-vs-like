7281293706f3985846c632a5cbbc13e7d555f4ba
Carl humphreys <humphreys.carl.d@gmail.com>
2024-06-17 15:08:46 +0100

    added health bar

--- Player.gd
+++ Player.gd
@@ -1,15 +1,24 @@
 extends CharacterBody2D
 class_name Player
+
+signal health_depleted
+
 @onready var happy_boo = $HappyBoo
+@onready var hurt_box = %HurtBox
+@onready var progress_bar = %ProgressBar
 
+@export var MAX_HEALTH: float
+@export var DAMAGE_RATE: float
 
-# Called when the node enters the scene tree for the first time.
+var health = 0.0
+
 func _ready():
-	pass # Replace with function body.
+	health = MAX_HEALTH
+	progress_bar.max_value = MAX_HEALTH
+	progress_bar.value = health
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	velocity = direction.normalized() * 1000
@@ -18,10 +27,17 @@ func _process(delta):
 		happy_boo.scale.x = -1
 	elif direction.x > 0:
 		happy_boo.scale.x = 1
+	move_and_slide()
 	
 	if velocity.length() > 0.0:
 		happy_boo.play_walk_animation()
 	else:
 		happy_boo.play_idle_animation()
-		
-	move_and_slide()
+	
+
+	var overlapping_mobs = hurt_box.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		progress_bar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
